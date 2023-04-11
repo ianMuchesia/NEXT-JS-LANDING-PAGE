1606cc36da807204c528ba8fb6e6780f8f71e250
ianMuchesia <ianmuchesia@yahoo.com>
2023-04-11 10:54:53 +0300

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav>
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+   </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil molestias laboriosam aperiam. Optio, repudiandae doloremque?</p>
+    </div>
+  )
+}
+
+export default About
