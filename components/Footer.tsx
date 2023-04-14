fc06a16010e29bd20f01c6d701d31b229b9cb708
ianMuchesia <ianmuchesia@yahoo.com>
2023-04-14 10:52:15 +0300

    deploy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,41 +1,65 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React, { useState } from "react";
 import styles from "../src/styles/footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit=(e:React.FormEvent)=>{
+    e.preventDefault()
+    setEmail("")
+  }
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
-      <div className={styles.address}>
-      <Image
-        src="/logo.png"
-        alt="logo"
-        width={67}
-        height={77}
-        className={styles.logo}
-      />
-      <h3>CARE SERVICES AND CONSULTANTS LTD</h3>
-      </div>
-      <div className={styles.usefulLinks}>
-        <h4>Useful Links</h4>
-        <ul>
-          <li>Project Info</li>
-          <li>Services</li>
-          <li>About</li>
-        </ul>
-      </div>
-      <div className={styles.signUp}>
-        <p>Keep me up to date with content and updates</p>
-        <input type="email"
-        placeholder='Enter Your email address' />
-        <button>submit</button>
-      </div>
+        <div className={styles.address}>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={67}
+            height={77}
+            className={styles.logo}
+          />
+
+          <h3>CARE SERVICES AND CONSULTANTS LTD</h3>
+        </div>
+        <div className={styles.usefulLinks}>
+          <h4>Useful Links</h4>
+          <ul>
+            <li>
+              <Link href="/contact" className={styles.footerLinks}>
+                Contact Info
+              </Link>
+            </li>
+            <li>
+              <Link href="/services" className={styles.footerLinks}>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className={styles.footerLinks}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <form className={styles.signUp} onSubmit={handleSubmit}>
+          <p>Keep me up to date with content and updates</p>
+          <input
+            type="email"
+            value={email}
+            placeholder="Enter Your email address"
+            onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setEmail(e.target.value)}}
+          />
+          <button>submit</button>
+        </form>
       </div>
       <div className={styles.groundFooter}>
-      &#169;	{new Date().getFullYear()}CARE SERVICES AND CONSULTANTS LTD
+        &#169; {new Date().getFullYear()}CARE SERVICES AND CONSULTANTS LTD
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
