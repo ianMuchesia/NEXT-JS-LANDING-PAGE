47a84228b8fa7a5f921dfe601f900be873dbe3af
ianMuchesia <ianmuchesia@yahoo.com>
2023-05-27 23:45:01 +0300

    sitemap manual

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,28 +1,29 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: 'https://careservicesandconsultantltd.com/', // Replace with your site URL
+  siteUrl: 'https://careservicesandconsultantltd.com', // Replace with your site URL
   generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ['/secret-page'], // Add any pages you want to exclude from the sitemap
+  exclude: [], // Add any pages you want to exclude from the sitemap
   robotsTxtOptions: {
-    additionalSitemaps: ['https://www.example.com/other-sitemap.xml'], // Add any additional sitemaps you have
+    additionalSitemaps: [], // Add any additional sitemaps you have
   },
   transform: async (config, path) => {
     const { lastmod } = config;
     const modifiedConfig = { ...config };
   
-    if (path === '/dynamic-page') {
-      // You can customize the lastmod value for specific pages
+    if (path === '/') {
+      // You can customize the lastmod value for the homepage
       modifiedConfig.lastmod = '2023-05-20T12:00:00.000Z';
     }
   
     return modifiedConfig;
   },
   
-  }
+};
+
   
   
   
