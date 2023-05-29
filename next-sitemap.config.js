fdd099e14659527c222c00c2270ca45c7548b12b
ianMuchesia <ianmuchesia@yahoo.com>
2023-05-29 13:27:58 +0300

    google search console

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: 'https://careservicesandconsultantltd.com', // Replace with your site URL
+  siteUrl: "https://careservicesandconsultantltd.com", // Replace with your site URL
   generateRobotsTxt: true,
-  changefreq: 'daily',
-  priority: 0.7,
 
-  exclude: ['/server-sitemap-index.xml'], // Add any pages you want to exclude from the sitemap
+  exclude: [], // Add any pages you want to exclude from the sitemap
   robotsTxtOptions: {
     additionalSitemaps: [], // Add any additional sitemaps you have
   },
- 
 };
-
-  
-  
-  
-  
-  
-  
